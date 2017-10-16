e94b0871491281b79dcf55929b7b6397dd202ff5
Michael Laccetti <michael@laccetti.com>
2017-10-16 16:26:49 -0400

    Some basic testing

--- test/test-integration.js
+++ test/test-integration.js
@@ -2,16 +2,15 @@
 
 const chai = require('chai');
 const expect = chai.expect;
-const sinon = require('sinon');
 chai.use(require('sinon-chai'));
 const redis = require('redis-mock');
-const request = require('supertest');
 const mockery = require('mockery');
+const sinon = require('sinon');
 const upath = require('upath');
 
-let Ravel, Routes, mapping, transaction, app;
+let Ravel, app, fs, stub;
 
-describe('Ravel MySQLProvider integration test', () => {
+describe('Ravel SequelizeProvider integration test', () => {
   beforeEach((done) => {
     process.removeAllListeners('unhandledRejection');
     // enable mockery
@@ -20,15 +19,20 @@ describe('Ravel MySQLProvider integration test', () => {
       warnOnReplace: false,
       warnOnUnregistered: false
     });
+
+    fs = require('fs');
+    mockery.registerMock('fs', fs);
+
     // scaffold basic Ravel app
     Ravel = require('ravel');
-    Routes = Ravel.Routes;
-    mapping = Routes.mapping;
-    transaction = Routes.transaction;
+    require('../lib/ravel-sequelize-provider')(Ravel); // eslint-disable-line new-cap, no-new
+    const SequelizeConfigurator = require('../lib/ravel-sequelize-provider').SequelizeConfigurator;
+
     mockery.registerMock('redis', redis);
     app = new Ravel();
-    app.set('log level', app.log.NONE);
-    new (require('../lib/ravel-sequelize-provider'))(app); // eslint-disable-line new-cap, no-new
+    new SequelizeConfigurator(app); // eslint-disable-line new-cap, no-new
+
+    app.set('log level', app.log.DEBUG);
     app.set('sequelize options', {
       user: 'ravel',
       password: 'ravel',
@@ -43,78 +47,42 @@ describe('Ravel MySQLProvider integration test', () => {
     process.removeAllListeners('unhandledRejection');
     mockery.deregisterAll();
     mockery.disable();
+    if (stub) {
+      stub.restore();
+    }
     done();
   });
 
-  it('should provide clients with a connection to query an existing MySQL database', (done) => {
-    class TestRoutes extends Routes {
-      constructor () {
-        super('/');
-      }
+  it('should expose models etc. from Sequelize to the app', (done) => {
+    stub = sinon.stub(fs, 'lstatSync').callsFake(function () {
+      return {
+        isDirectory: function () { return true; }
+      };
+    });
 
-      @mapping(Routes.GET, 'test')
-      testHandler (ctx) {
-        expect(ctx).to.have.a.property('transaction').that.is.an('object');
-        expect(ctx.transaction).to.have.a.property('mysql').that.is.an('object');
-        return new Promise((resolve, reject) => {
-          ctx.transaction.mysql.query('SELECT 1 AS col', (err, rows) => {
-            if (err) {
-              return reject(err);
-            }
-            ctx.body = rows[0];
-            resolve(rows[0]);
-          });
-        });
-      }
-    }
-    mockery.registerMock(upath.join(app.cwd, 'routes'), TestRoutes);
-    app.routes('routes');
+    const TestModel = (sequelize, Sequelize) => {
+      return sequelize.define('task', {
+        title: Sequelize.STRING,
+        description: Sequelize.TEXT,
+        deadline: Sequelize.DATE
+      });
+    };
+
+    mockery.registerMock(upath.join(app.cwd, 'models/test.js'), TestModel);
+    app.models('./models');
     app.init();
     app.emit('pre listen');
 
-    request.agent(app.server)
-      .get('/test')
-      .expect(200, JSON.stringify({
-        col: '1'
-      }))
-      .end((err) => {
-        app.close();
-        done(err);
-      });
-  });
-
-  it('should trigger a rollback when a query fails', (done) => {
-    let spy;
-    class TestRoutes extends Routes {
-      constructor () {
-        super('/');
-      }
-
-      @transaction
-      @mapping(Routes.GET, 'test')
-      testHandler (ctx) {
-        expect(ctx).to.have.a.property('transaction').that.is.an('object');
-        expect(ctx.transaction).to.have.a.property('mysql').that.is.an('object');
-        spy = sinon.spy(ctx.transaction.mysql, 'rollback');
-        return Promise.reject(new Error());
-      }
+    try {
+      const models = app.getModels();
+      expect(models).to.not.be.null;
+      expect(models).to.have.a.property('size');
+      expect(models.size).to.be(1);
+    } catch (err) {
+      throw err;
+    } finally {
+      app.close();
+      done();
     }
-    mockery.registerMock(upath.join(app.cwd, 'routes'), TestRoutes);
-    app.routes('routes');
-    app.init();
-    app.emit('pre listen');
-
-    request.agent(app.server)
-      .get('/test')
-      .end((err) => {
-        try {
-          expect(spy).to.have.been.called;
-          done(err);
-        } catch (e) {
-          done(e);
-        } finally {
-          app.close();
-        }
-      });
   });
 });
